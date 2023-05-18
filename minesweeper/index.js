d7226dd31290a71577efd111ae33f736df17e625
“Maria <mashabogdanoff@gmail.com>
2023-05-18 16:08:17 +0200

    refactor: rename and extract board methods

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -14,11 +14,11 @@ class Board {
             [1, -1],
             [1, 0],
             [1, 1]
-        ]
+        ];
         this.boardElement.addEventListener("click", this.checkClickedCell);
         this.boardElement.addEventListener("contextmenu", (e) => {
             e.preventDefault();
-            this.onDoubleClick(e);
+            this.flagClickedCell(e);
         });
     }
 
@@ -33,14 +33,12 @@ class Board {
         }
         return board;
     }
-
     createBoardElement() {
         let boardElement = document.createElement("section");
         boardElement.classList.add("board");
         document.body.append(boardElement);
         return boardElement;
     }
-
     localizeMines() {
         for (let i = 0; i < this.minesCount; i++) {
             const randomNumber = Math.floor(Math.random() * (this.rowCount * this.rowCount));
@@ -60,19 +58,24 @@ class Board {
         if (chosenCell.mine) {
             chosenCell.opened = true;
             chosenCell.cellElement.classList.add("board__cell_mined");
-            this.endTheGame();
+            // this.endTheGame();
         } else {
-            this.getMinedNeighboursCount(row, column, chosenCell);
+            this.openAndCheckNeighbours(row, column, chosenCell);
         }
-
     }
-
-    getMinedNeighboursCount(row, column, chosenCell) {
-        // Add opened cell style
+    openAndCheckNeighbours(row, column, chosenCell) {
         chosenCell.cellElement.classList.add("board__cell_opened");
         chosenCell.opened = true;
 
-        // Count mined neighbours
+        const counter = this.countMinedNeighbours(row, column);
+
+        if (counter === 0) {
+           this.checkNeighbours(row, column);
+        } else {
+            this.board[row][column].cellElement.innerHTML = `${counter}`;
+        }
+    }
+    countMinedNeighbours(row, column) {
         let counter = 0;
 
         for (let direction of this.closestCellsDirections) {
@@ -84,24 +87,20 @@ class Board {
                 counter += 1;
             }
         }
+        return counter;
+    }
+    checkNeighbours(row, column) {
+        for (const direction of this.closestCellsDirections) {
+            const rowNumber = row + direction[0];
+            const colNumber = column + direction[1];
 
-        // Show number of mined neighbours or check each neighbour
-        if (counter === 0) {
-            for (const direction of this.closestCellsDirections) {
-                const rowNumber = row + direction[0];
-                const colNumber = column + direction[1];
-
-                if (rowNumber >= 0 && colNumber >= 0 && rowNumber <= this.rowCount - 1 && colNumber <= this.rowCount - 1
-                    && !this.board[rowNumber][colNumber].opened) {
-                    this.getMinedNeighboursCount(rowNumber, colNumber, this.board[rowNumber][colNumber]);
-                }
+            if (rowNumber >= 0 && colNumber >= 0 && rowNumber <= this.rowCount - 1 && colNumber <= this.rowCount - 1
+                && !this.board[rowNumber][colNumber].opened) {
+                this.openAndCheckNeighbours(rowNumber, colNumber, this.board[rowNumber][colNumber]);
             }
-        } else {
-            this.board[row][column].cellElement.innerHTML = `${counter}`;
         }
     }
-
-    onDoubleClick = e => {
+    flagClickedCell = e => {
         // todo: extract method
         const row = e.target.id.split("-")[0];
         const column = e.target.id.split("-")[1];
@@ -117,7 +116,6 @@ class Board {
             chosenCell.flagged = true;
         }
     }
-
     endTheGame() {
         // todo: add time and steps count, fix overlay
         const modal = document.createElement("div");
@@ -141,7 +139,6 @@ class Cell {
         this.board = board;
         this.cellElement = this.createCell();
     }
-
     createCell() {
         let cell = document.createElement("div");
         cell.classList.add("board__cell");
@@ -150,7 +147,6 @@ class Cell {
         this.board.boardElement.append(cell);
         return cell;
     }
-
     addMine() {
         this.mine = true;
     }
