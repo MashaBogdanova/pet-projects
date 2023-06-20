11f481e8bed43f606c57e8bde4e94b30c26c8400
“Maria <mashabogdanoff@gmail.com>
2023-06-20 17:22:01 +0200

    feat: add styles and assets

--- css-selectors/src/index.ts
+++ css-selectors/src/index.ts
@@ -24,12 +24,12 @@ const levelsData: Array<ILevelParams> = [
         itemsStyle: ['asdf'],
         levelHint: {
             header: 'No worries, you\'ve got this!',
-            text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to. \n' +
-                'Here, the "p" is the selector (selects all <p> elements) and applies the margin-bottom style. \n' +
-                'To play, type in a CSS selector in the editor below to select the correct items on the table. \n' +
-                'If you get it right, you\'ll advance to the next level. \n' +
+            text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
+                'Here, the "p" is the selector (selects all <p> elements) and applies the margin-bottom style.' +
+                'To play, type in a CSS selector in the editor below to select the correct items on the table.' +
+                'If you get it right, you\'ll advance to the next level.' +
                 'Hover over the items on the table to see their HTML markup.',
-            solution: 'p {\n' + '   margin-bottom: 12px;\n' + '}'
+            solution: 'p { margin-bottom: 12px; }'
         }
     }
 ]
