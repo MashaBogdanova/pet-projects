c11896f17fb7721c0c1a09ad0eeeeef41f98100f
“Maria <mashabogdanoff@gmail.com>
2023-06-22 15:49:32 +0200

    feat: add animation to target elements

--- css-selectors/src/data/data.ts
+++ css-selectors/src/data/data.ts
@@ -11,8 +11,8 @@ export const levelsData: Array<ILevelParams> = [
             exampleText: 'div selects all <div> elements.'
         },
         itemsSet:
-            '<div class="plate"></div>' +
-            '<div class="plate"></div>',
+            '<div class="plate target"></div>' +
+            '<div class="plate target"></div>',
         levelHint: {
             title: 'No worries, you\'ve got this!',
             text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
@@ -36,7 +36,7 @@ export const levelsData: Array<ILevelParams> = [
             exampleText: '#cool selects any element with id="cool"'
         },
         itemsSet:
-            '<div class="plate plate_fancy"></div>' +
+            '<div class="plate plate_fancy target"></div>' +
             '<div class="plate"></div>' +
             '<div class="plate"></div>',
         levelHint: {
@@ -63,7 +63,7 @@ export const levelsData: Array<ILevelParams> = [
         },
         itemsSet:
             '<div class="plate plate_fancy"></div>' +
-            '<div class="plate"><div class="yummy yummy_top"></div></div>' +
+            '<div class="plate"><div class="yummy yummy_top target"></div></div>' +
             '<div class="yummy"></div>',
         levelHint: {
             title: 'No worries, you\'ve got this!',
@@ -75,7 +75,7 @@ export const levelsData: Array<ILevelParams> = [
                 'Hover over the items on the table to see their HTML markup.' +
                 'Get help with selectors on the right! →'
         },
-        solution: 'plate apple',
+        solution: 'plate cupcake',
         html: '<div class="container">\n  <plate />\n  <plate>\n    <cupcake />\n  </plate>\n  <cupcake />\n</div>'
     },
     {
@@ -89,8 +89,8 @@ export const levelsData: Array<ILevelParams> = [
         },
         itemsSet:
             '<div class="yummy yummy_cupcake"></div>' +
-            '<div class="yummy yummy_cupcake yummy_small"></div>' +
-            '<div class="plate"><div class="yummy yummy_cupcake yummy_small yummy_top"></div></div>' +
+            '<div class="yummy yummy_cupcake yummy_small target"></div>' +
+            '<div class="plate"><div class="yummy yummy_cupcake yummy_small yummy_top target"></div></div>' +
             '<div class="plate"></div>',
         levelHint: {
             title: 'No worries, you\'ve got this!',
@@ -121,9 +121,9 @@ export const levelsData: Array<ILevelParams> = [
         },
         itemsSet:
             '<div class="plate plate_fancy"></div>' +
-            '<div class="yummy yummy_strawberry"></div>' +
-            '<div class="plate"><div class="yummy yummy_cupcake yummy_top"></div></div>' +
-            '<div class="plate plate_fancy"><div class="yummy yummy_cake yummy_top"></div></div>' +
+            '<div class="yummy yummy_strawberry target"></div>' +
+            '<div class="plate"><div class="yummy yummy_cupcake yummy_top target"></div></div>' +
+            '<div class="plate plate_fancy"><div class="yummy yummy_cake yummy_top target"></div></div>' +
             '<div class="plate"></div>',
         levelHint: {
             title: 'No worries, you\'ve got this!',
@@ -155,11 +155,11 @@ export const levelsData: Array<ILevelParams> = [
             exampleText: '* selects all elements.'
         },
         itemsSet:
-            '<div class="yummy yummy_strawberry"></div>' +
-            '<div class="plate"><div class="yummy yummy_cupcake yummy_top yummy_small"></div></div>' +
-            '<div class="plate plate_fancy"></div>' +
-            '<div class="plate plate_fancy"><div class="yummy yummy_cupcake yummy_top"></div></div>' +
-            '<div class="plate"></div>',
+            '<div class="yummy yummy_strawberry target"></div>' +
+            '<div class="plate target"><div class="yummy yummy_cupcake yummy_top yummy_small target"></div></div>' +
+            '<div class="plate plate_fancy target"></div>' +
+            '<div class="plate plate_fancy target"><div class="yummy yummy_cupcake yummy_top target"></div></div>' +
+            '<div class="plate target"></div>',
         levelHint: {
             title: 'No worries, you\'ve got this!',
             text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
@@ -190,10 +190,10 @@ export const levelsData: Array<ILevelParams> = [
             exampleText: 'p * selects every element inside all <p> elements.'
         },
         itemsSet:
-            '<div class="plate"><div class="yummy yummy_cake yummy_top"></div></div>' +
-            '<div class="plate"><div class="yummy yummy_strawberry yummy_top"></div></div>' +
+            '<div class="plate"><div class="yummy yummy_cake yummy_top target"></div></div>' +
+            '<div class="plate"><div class="yummy yummy_strawberry yummy_top target"></div></div>' +
             '<div class="yummy yummy_cake"></div>' +
-            '<div class="plate"><div class="yummy yummy_cupcake yummy_top yummy_small"></div></div>',
+            '<div class="plate"><div class="yummy yummy_cupcake yummy_top yummy_small target"></div></div>',
         levelHint: {
             title: 'No worries, you\'ve got this!',
             text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
@@ -211,6 +211,157 @@ export const levelsData: Array<ILevelParams> = [
             '  <plate>\n    <strawberry />\n  </plate>\n' +
             '  <cake />\n' +
             '  <plate />\n    <cupcake class="small"/>\n  </plate>\n' +
+            '</div>'
+    },
+    {
+        levelNumber: 7,
+        instruction: 'Select every strawberry that\'s next to a plate',
+        levelRules: {
+            title: 'Adjacent Sibling Selector',
+            subtitle: 'Select an element that directly follows another element',
+            text:
+                'This selects all B elements that directly follow A. Elements that follow one another are called ' +
+                'siblings. They\'re on the same level, or depth.\n' +
+                'In the HTML markup for this level, elements that have the same indentation are siblings.',
+            exampleText: 'p + .intro selects every element with class="intro" that directly follows a <p>.'
+        },
+        itemsSet:
+            '<div class="plate plate_fancy"><div class="yummy yummy_strawberry yummy_top yummy_small"></div></div>' +
+            '<div class="plate"></div>' +
+            '<div class="yummy yummy_strawberry yummy_small target"></div>' +
+            '<div class="plate"></div>' +
+            '<div class="yummy yummy_strawberry target"></div>' +
+            '<div class="yummy yummy_strawberry yummy_small"></div>' +
+            '<div class="yummy yummy_strawberry yummy_small"></div>',
+            levelHint: {
+            title: 'No worries, you\'ve got this!',
+            text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
+                'p { margin-bottom: 12px; }' +
+                'Here, the "p" is the selector (selects all <p> elements) and applies the margin-bottom style.' +
+                'To play, type in a CSS selector in the editor below to select the correct items on the table.' +
+                'If you get it right, you\'ll advance to the next level.' +
+                'Hover over the items on the table to see their HTML markup.' +
+                'Get help with selectors on the right! →'
+        },
+        solution: 'plate + strawberry',
+        html:
+            '<div class="container">\n' +
+            '  <plate>\n    <strawberry class="small" />\n  </plate>\n' +
+            '  <plate />\n'+
+            '  <strawberry class="small" />\n' +
+            '  <plate />\n'+
+            '  <strawberry />\n' +
+            '  <strawberry class="small" />\n' +
+            '  <strawberry class="small" />\n' +
+            '</div>'
+    },
+    {
+        levelNumber: 8,
+        instruction: 'Select the 3rd plate',
+        levelRules: {
+            title: 'Nth Child Pseudo-selector',
+            subtitle: 'Select an element by its order in another element',
+            text:
+                'Selects the nth (Ex: 1st, 3rd, 12th etc.) child element in another element.',
+            exampleText: 'div p:nth-child(2) selects the second <p> in every <div>.'
+        },
+        itemsSet:
+            '<div class="plate plate_fancy"></div>' +
+            '<div class="plate plate_fancy"></div>' +
+            '<div class="plate plate_fancy target"></div>' +
+            '<div class="plate"></div>',
+        levelHint: {
+            title: 'No worries, you\'ve got this!',
+            text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
+                'p { margin-bottom: 12px; }' +
+                'Here, the "p" is the selector (selects all <p> elements) and applies the margin-bottom style.' +
+                'To play, type in a CSS selector in the editor below to select the correct items on the table.' +
+                'If you get it right, you\'ll advance to the next level.' +
+                'Hover over the items on the table to see their HTML markup.' +
+                'Get help with selectors on the right! →'
+        },
+        solution: '.container plate:nth-child(3)',
+        html:
+            '<div class="container">\n' +
+            '  <plate class="fancy"/>\n'+
+            '  <plate class="fancy"/>\n'+
+            '  <plate class="fancy"/>\n'+
+            '  <plate />\n'+
+            '</div>'
+    },
+    {
+        levelNumber: 9,
+        instruction: 'Select the empty plates',
+        levelRules: {
+            title: 'Empty Selector',
+            subtitle: 'Select elements that don\'t have children',
+            text: 'Selects elements that don\'t have any other elements inside of them.',
+            exampleText: 'div:empty selects all empty div elements.'
+        },
+        itemsSet:
+            '<div class="plate plate_fancy"><div class="yummy yummy_top"></div></div>' +
+            '<div class="plate target"></div>' +
+            '<div class="plate plate_fancy target"></div>' +
+            '<div class="plate"><div class="yummy yummy_strawberry yummy_top"></div></div>',
+            levelHint: {
+            title: 'No worries, you\'ve got this!',
+            text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
+                'p { margin-bottom: 12px; }' +
+                'Here, the "p" is the selector (selects all <p> elements) and applies the margin-bottom style.' +
+                'To play, type in a CSS selector in the editor below to select the correct items on the table.' +
+                'If you get it right, you\'ll advance to the next level.' +
+                'Hover over the items on the table to see their HTML markup.' +
+                'Get help with selectors on the right! →'
+        },
+        solution: 'plate:empty',
+        html:
+            '<div class="container">\n' +
+            '  <plate class="fancy">\n    <cupcake />\n  </plate>\n' +
+            '  <plate />\n'+
+            '  <plate class="fancy" />\n'+
+            '  <plate>\n    <strawberry />\n  </plate>\n' +
+            '</div>'
+    },
+    {
+        levelNumber: 10,
+        instruction: 'Select every strawberry that\'s next to a plate',
+        levelRules: {
+            title: 'Super challenge!',
+            subtitle: 'Select an element that directly follows another element',
+            text:
+                'This selects all B elements that directly follow A. Elements that follow one another are called ' +
+                'siblings. They\'re on the same level, or depth.\n' +
+                'In the HTML markup for this level, elements that have the same indentation are siblings.',
+            exampleText: 'p + .intro selects every element with class="intro" that directly follows a <p>'
+        },
+        itemsSet:
+            '<div class="plate plate_fancy"><div class="yummy yummy_strawberry yummy_top yummy_small"></div></div>' +
+            '<div class="plate"></div>' +
+            '<div class="yummy yummy_strawberry yummy_small"></div>' +
+            '<div class="plate"></div>' +
+            '<div class="yummy yummy_strawberry"></div>' +
+            '<div class="yummy yummy_strawberry yummy_small"></div>' +
+            '<div class="yummy yummy_strawberry yummy_small"></div>',
+        levelHint: {
+            title: 'No worries, you\'ve got this!',
+            text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
+                'p { margin-bottom: 12px; }' +
+                'Here, the "p" is the selector (selects all <p> elements) and applies the margin-bottom style.' +
+                'To play, type in a CSS selector in the editor below to select the correct items on the table.' +
+                'If you get it right, you\'ll advance to the next level.' +
+                'Hover over the items on the table to see their HTML markup.' +
+                'Get help with selectors on the right! →'
+        },
+        solution: 'plate + strawberry',
+        html:
+            '<div class="container">\n' +
+            '  <plate>\n    <strawberry class="small" />\n  </plate>\n' +
+            '  <plate />\n'+
+            '  <strawberry class="small" />\n' +
+            '  <plate />\n'+
+            '  <strawberry />\n' +
+            '  <strawberry class="small" />\n' +
+            '  <strawberry class="small" />\n' +
             '</div>'
     }
 ]
--- css-selectors/src/types/types.ts
+++ css-selectors/src/types/types.ts
@@ -18,4 +18,4 @@ type hintType = {
     title: string;
     text: string;
 }
-export type levelCountType = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+export type levelCountType = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
