42bc2be63da67b42ba3f3dd33fbe63777488566f
“Maria <mashabogdanoff@gmail.com>
2023-06-22 10:20:16 +0200

    feat: add more levels data

--- css-selectors/src/data/data.ts
+++ css-selectors/src/data/data.ts
@@ -10,7 +10,9 @@ export const levelsData: Array<ILevelParams> = [
             text: 'Selects all elements of type A. Type refers to the type of tag, so div, p and ul are all different element types.',
             exampleText: 'div selects all <div> elements.'
         },
-        itemsSet: '<div class="plate"></div><div class="plate"></div>',
+        itemsSet:
+            '<div class="plate"></div>' +
+            '<div class="plate"></div>',
         levelHint: {
             title: 'No worries, you\'ve got this!',
             text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
@@ -33,7 +35,10 @@ export const levelsData: Array<ILevelParams> = [
             text: 'Selects the element with a specific id. You can also combine the ID selector with the type selector.',
             exampleText: '#cool selects any element with id="cool"'
         },
-        itemsSet: '<div class="plate plate_fancy"></div><div class="plate"></div><div class="plate"></div>',
+        itemsSet:
+            '<div class="plate plate_fancy"></div>' +
+            '<div class="plate"></div>' +
+            '<div class="plate"></div>',
         levelHint: {
             title: 'No worries, you\'ve got this!',
             text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
@@ -56,7 +61,10 @@ export const levelsData: Array<ILevelParams> = [
             text: 'Selects all B inside of A. B is called a descendant because it is inside of another element.',
             exampleText: '#fancy span selects any <span> elements that are \inside of the element with id="fancy"'
         },
-        itemsSet: '<div class="plate plate_fancy"></div><div class="plate"><div class="yummy yummy_top"></div></div><div class="yummy"></div>',
+        itemsSet:
+            '<div class="plate plate_fancy"></div>' +
+            '<div class="plate"><div class="yummy yummy_top"></div></div>' +
+            '<div class="yummy"></div>',
         levelHint: {
             title: 'No worries, you\'ve got this!',
             text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
@@ -72,14 +80,18 @@ export const levelsData: Array<ILevelParams> = [
     },
     {
         levelNumber: 3,
-        instruction: 'Select the small cakes',
+        instruction: 'Select the small cupcakes',
         levelRules: {
             title: 'Class Selector',
             subtitle: 'Select elements by their class',
             text: 'The class selector selects all elements with that class attribute. Elements can only have one ID, but many classes.',
             exampleText: '.neato selects all elements with class="neato"'
         },
-        itemsSet: '<div class="yummy yummy_cake"></div><div class="yummy yummy_cake yummy_small"></div><div class="plate"><div class="yummy yummy_cake yummy_small yummy_top"></div></div><div class="plate"></div>',
+        itemsSet:
+            '<div class="yummy yummy_cupcake"></div>' +
+            '<div class="yummy yummy_cupcake yummy_small"></div>' +
+            '<div class="plate"><div class="yummy yummy_cupcake yummy_small yummy_top"></div></div>' +
+            '<div class="plate"></div>',
         levelHint: {
             title: 'No worries, you\'ve got this!',
             text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
@@ -93,9 +105,9 @@ export const levelsData: Array<ILevelParams> = [
         solution: '.small',
         html:
             '<div class="container">\n' +
-            '  <cake />\n' +
-            '  <cake class="small" />\n' +
-            '  <plate>\n    <cake class="small" />\n  </plate>\n' +
+            '  <cupcake />\n' +
+            '  <cupcake class="small" />\n' +
+            '  <plate>\n    <cupcake class="small" />\n  </plate>\n' +
             '  </plate>\n' +
             '</div>'},
     {
@@ -127,10 +139,78 @@ export const levelsData: Array<ILevelParams> = [
         html:
             '<div class="container">\n' +
             '  <plate class="fancy"/>\n' +
-            '  <straeberry />\n' +
+            '  <strawberry />\n' +
             '  <plate>\n    <cupcake />\n  </plate>\n' +
             '  <plate class="fancy">\n    <cake />\n  </plate>\n' +
             '  </plate>\n' +
+            '</div>'
+    },
+    {
+        levelNumber: 5,
+        instruction: 'Select all the things!',
+        levelRules: {
+            title: 'The Universal Selector',
+            subtitle: 'You can select everything!',
+            text: 'You can select all elements with the universal selector!',
+            exampleText: '* selects all elements.'
+        },
+        itemsSet:
+            '<div class="yummy yummy_strawberry"></div>' +
+            '<div class="plate"><div class="yummy yummy_cupcake yummy_top yummy_small"></div></div>' +
+            '<div class="plate plate_fancy"></div>' +
+            '<div class="plate plate_fancy"><div class="yummy yummy_cupcake yummy_top"></div></div>' +
+            '<div class="plate"></div>',
+        levelHint: {
+            title: 'No worries, you\'ve got this!',
+            text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
+                'p { margin-bottom: 12px; }' +
+                'Here, the "p" is the selector (selects all <p> elements) and applies the margin-bottom style.' +
+                'To play, type in a CSS selector in the editor below to select the correct items on the table.' +
+                'If you get it right, you\'ll advance to the next level.' +
+                'Hover over the items on the table to see their HTML markup.' +
+                'Get help with selectors on the right! →'
+        },
+        solution: '*',
+        html:
+            '<div class="container">\n' +
+            '  <strawberry />\n' +
+            '  <plate>\n    <cupcake class="small"/>\n  </plate>\n' +
+            '  <plate class="fancy" />\n' +
+            '  <plate class="fancy" />\n    <cupcake />\n  </plate>\n' +
+            '  </plate>\n' +
+            '</div>'
+    },
+    {
+        levelNumber: 6,
+        instruction: 'Select everything on a plate',
+        levelRules: {
+            title: 'The Universal Selector',
+            subtitle: 'Combine the Universal Selector',
+            text: 'This selects all elements inside of A.',
+            exampleText: 'p * selects every element inside all <p> elements.'
+        },
+        itemsSet:
+            '<div class="plate"><div class="yummy yummy_cake yummy_top"></div></div>' +
+            '<div class="plate"><div class="yummy yummy_strawberry yummy_top"></div></div>' +
+            '<div class="yummy yummy_cake"></div>' +
+            '<div class="plate"><div class="yummy yummy_cupcake yummy_top yummy_small"></div></div>',
+        levelHint: {
+            title: 'No worries, you\'ve got this!',
+            text: 'You\'re about to learn CSS Selectors! Selectors are how you pick which element to apply styles to.' +
+                'p { margin-bottom: 12px; }' +
+                'Here, the "p" is the selector (selects all <p> elements) and applies the margin-bottom style.' +
+                'To play, type in a CSS selector in the editor below to select the correct items on the table.' +
+                'If you get it right, you\'ll advance to the next level.' +
+                'Hover over the items on the table to see their HTML markup.' +
+                'Get help with selectors on the right! →'
+        },
+        solution: 'plate *',
+        html:
+            '<div class="container">\n' +
+            '  <plate>\n    <cake />\n  </plate>\n' +
+            '  <plate>\n    <strawberry />\n  </plate>\n' +
+            '  <cake />\n' +
+            '  <plate />\n    <cupcake class="small"/>\n  </plate>\n' +
             '</div>'
     }
 ]
