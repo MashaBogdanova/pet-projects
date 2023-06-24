2449b7f6a18197d7de35d7d49c6e551589e8a6d4
“Maria <mashabogdanoff@gmail.com>
2023-06-24 22:18:05 +0200

    fix: add styles to nav title on small screen

--- css-selectors/src/index.ts
+++ css-selectors/src/index.ts
@@ -2,7 +2,6 @@ import { Game } from './modules/game';
 import { levelCountType } from './types/types';
 
 const currentLevel = localStorage.getItem('level');
-console.log(currentLevel);
 if (!currentLevel) {
     localStorage.setItem('level', '0');
 }
