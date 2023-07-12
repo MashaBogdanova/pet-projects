be9679af6f9889934ab22f07844e9a44ae8f0f0f
“Maria <mashabogdanoff@gmail.com>
2023-07-12 19:32:10 +0200

    chore: add typescript

--- async-race/webpack.config.js
+++ async-race/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './css-selectors/src/index.ts',
+    entry: './src/index.ts',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './dist'),
